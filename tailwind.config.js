14039794de81d79ff879d5151df2a438d0e59402
Reda-BENMAKDAD <redabenmakdad@gmail.com>
2023-05-21 02:58:49 +0100

    customized breeze styles and changed logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'mygold': "#F7BF4F",
+            }
         },
     },
 
