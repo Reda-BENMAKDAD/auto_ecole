f721319476cf922dd7d076c0aae38fdad4537868
Reda-BENMAKDAD <redabenmakdad@gmail.com>
2023-05-22 01:59:54 +0100

    added favicon

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
