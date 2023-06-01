fd16edbd2a5302ebcd9fb5f49e5126d9d6f3f327
Reda-BENMAKDAD <redabenmakdad@gmail.com>
2023-06-01 10:12:30 +0100

    added some roles features

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,5 +1,6 @@
-@props(['disabled' => false])
+@props(['disabled' => false,
+        'cursor' => "pointer"])
 
-<button {{ $disabled ? 'disabled' : '' }} {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center bg-mygold px-4 py-2 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:hover:brightness-110 focus:hover:brightness-110 active:hover:brightness-110 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+<button {{ $disabled ? 'disabled' : '' }} {{ $attributes->merge(['type' => 'submit', 'class' => 'ursor-not-allowed inline-flex items-center bg-mygold px-4 py-2 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:hover:brightness-110 focus:hover:brightness-110 active:hover:brightness-110 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }} style="cursor: {{ $cursor  }}">
     {{ $slot }}
 </button>
--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,5 @@
-@props(['disabled' => false])
+@props(['disabled' => false,
+        'cursor' => "pointer"
+])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<input style="cursor: {{ $cursor }}"  {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
